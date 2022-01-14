1afc4b59552aeab3a8e3510c5982c20a4d0451e2
matheusluc <matheus.supervisor@gmail.com>
2022-01-13 21:24:43 -0300

    Finalizando o programa para adivinhação de palavra

--- guessing-game.py
+++ guessing-game.py
@@ -6,13 +6,26 @@ while True:
     typedWord = input('Digite uma letra: ')
 
     if len(typedWord) > 1:
-        print('Não vale, você precisa digitar apenas uma paravra!!')
+        print('Não vale, você precisa digitar apenas uma paravra!!\n')
         continue
 
-    letra = typedWord
-    for letra in secretWord:
-        if letra == secretWord:
-            print('essa letra está na paravra secreta!!')
+    typedExistsInTheSecretWord.append(typedWord)
+    if typedWord in secretWord:
+        print('Essa letra está na paravra secreta!!')
 
+    else:
+        print('Essa letra não está na palavra secreta!!')
+        typedExistsInTheSecretWord.pop()
+
+    temporary_letter = ''
+    for secret_word in secretWord:
+        if secret_word in typedExistsInTheSecretWord:
+            temporary_letter += secret_word
         else:
-            print('Essa letra não está na palavra secreta!!')
+            temporary_letter += "*"
+
+    if temporary_letter == secretWord:
+        print(f'Parabens, você acertou a palavra secreta: {temporary_letter}\n')
+        break
+    else:
+        print(f'A palavra secreta ainda não está batendo, porem olha como ela está: {temporary_letter} tente mais um pouco\n')
